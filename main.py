3758f1c5ceb5b052db3fa216fbdf7ce09d9aafe9
FlaggedATX <renatosegundo840@gmail.com>
2026-08-30 20:47:34 -0300

    feat: Add main terminal menu allowing for programn use

---
+++ main.py
@@ -0,0 +1,22 @@
+from scripts.check_area import consulta
+
+
+
+def main():
+    running = True
+    while running:
+        print("-----------------")
+        print("  CEP-GEOFINDER ")
+        print("------------------")
+        latitude = input("Enter Latitude: ")
+        longitude = input("Enter Longitude: ")
+        radius = float(input("Enter Radius: "))
+        result = consulta(latitude, longitude, radius)
+        count = 0
+        for i in result:
+            count += 1
+            print(i)
+        print(f"{count} CEPs were found in that radius\n")
+
+if __name__ == "__main__":
+    main()
